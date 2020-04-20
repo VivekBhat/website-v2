84777d0f7789b5addfd290d939fc597d93676134
Vivek Bhat <VivekBhat@users.noreply.github.com>
2020-04-20 12:59:09 -0700

    fixed issue and refactored code (#7)

--- src/app/services/projects/projects.service.ts
+++ src/app/services/projects/projects.service.ts
@@ -1,6 +1,6 @@
 import {Observable} from 'rxjs';
 import {Project} from '../../models/project/project';
-import {Injectable, InjectionToken} from '@angular/core';
+import {InjectionToken} from '@angular/core';
 
 export const ProjectsServiceToken = new InjectionToken<ProjectsService>('ProjectsService');
 
@@ -14,3 +14,46 @@ export interface ProjectsService {
 
 }
 
+
+export const ossProjects = [
+  {
+    title: 'Expertiza Docker Image',
+    technology: 'Docker, Bash scripts, GitHub',
+    imgName: 'expertiza_1',
+    gitHubLink: 'expertiza',
+    readmeLink: '',
+  }, {
+    title: 'Expertiza Log Visualization',
+    technology: 'Elasticache, Logstash, Kibana',
+    imgName: 'expertiza_2',
+    gitHubLink: 'expertiza',
+    readmeLink: '',
+  }, {
+    title: 'Expertiza Wiki Page',
+    technology: 'Documentation and Wikipedia styling',
+    imgName: 'expertiza_3',
+    gitHubLink: 'expertiza',
+    readmeLink: ''
+  }
+];
+export const otherProjects = [
+  {
+    title: 'Kubernetes Local Deployments',
+    imgName: 'other_1',
+    technology: 'Kubernetes, Docker, Vagrant',
+    gitHubLink: 'kubernetes-deployment',
+    readmeLink: 'https://raw.githubusercontent.com/VivekBhat/kubernetes-deployment/master/Readme.md'
+  }, {
+    title: 'Serverless Thumbnail Generator',
+    imgName: 'other_thumbnail_generator',
+    technology: 'Serverless, AWS, Vagrant',
+    gitHubLink: 'serverless-thumbnail-aws',
+    readmeLink: 'https://raw.githubusercontent.com/VivekBhat/serverless-thumbnail-aws/master/Readme.md'
+  }, {
+    title: 'Serverless REST API Using AWS',
+    imgName: 'serverless-nodejs-dynamodb-rest-api',
+    technology: 'AWS API Gateway, Lambda, DynamoDB, NodeJS',
+    gitHubLink: 'serverless-nodejs-dynamodb-rest-api',
+    readmeLink: 'https://raw.githubusercontent.com/VivekBhat/serverless-nodejs-dynamodb-rest-api/master/Readme.md'
+  }
+];
