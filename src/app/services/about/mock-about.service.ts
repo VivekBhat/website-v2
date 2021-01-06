d2c7d5146ed194be89d98c7eb778953ab0a8cd92
Bhat, Vivek <vivek.bhat@intel.com>
2021-01-06 16:11:29 +0530

    about page updated

--- src/app/services/about/mock-about.service.ts
+++ src/app/services/about/mock-about.service.ts
@@ -1,16 +1,18 @@
 import {AboutService} from './about.service';
 import {Observable, of} from 'rxjs';
-import { Injectable } from "@angular/core";
+import {Injectable} from '@angular/core';
 
 @Injectable()
 export class MockAboutService implements AboutService {
 
   getAbout(): Observable<string[]> {
     const about =
-      'I am an AWS Certified Solutions Architect and Developer Associate specializing in three-tier web applications. \n' +
-      'I have experience with all stages of the development cycle for dynamic web projects, ' +
-      '2.5 years of progressive experience in Angular, HTML, CSS, Typescript development. ' +
-      'We work in an Agile environment, possess creative design thinking skills and dedicated to work effectively in ' +
+      'I am an AWS Certified Developer and Solutions Architect with experience designing, developing and deploying AI and Big Data ' +
+      'solutions in an Agile environment. I am an avid clean coder who is adept at rapid prototyping for fast turnaround times in POCs.\n' +
+      'I have rich experience with all stages of the software development life cycle, ' +
+      'more than 3 years of progressive experience in Java, Apache Hadoop, Big Data, HDFS, HBase, NiFi, Angular, HTML, CSS ' +
+      'and Typescript development. \n' +
+      'I have been working in an Agile environment and possess creative design thinking skills and dedicated to work effectively in ' +
       'dynamic environments. \n' +
       'In my free time I love listening to music, playing soccer, cricket, swimming and go hiking. ' +
       'I am a lover of innovation and everything that would increase my set of skills and knowledge. \n' +
