63475345670510684f1980fb963360a025f75aa5
Vivek Bhat <vivekbhat@live.com>
2021-09-21 11:15:20 -0700

    some intro added

--- src/app/services/about/mock-about.service.ts
+++ src/app/services/about/mock-about.service.ts
@@ -7,10 +7,11 @@ export class MockAboutService implements AboutService {
 
   getAbout(): Observable<string[]> {
     const about =
-      'I am an AWS Certified Developer and Solutions Architect with experience designing, developing and deploying AI and Big Data ' +
-      'solutions in an Agile environment. I am an avid clean coder who is adept at rapid prototyping for fast turnaround times in POCs.\n' +
+      'I am a software engineer working for Microsoft and write clean, scalable and tolerant microservice for the Wide Area Networking team of Azure. '+
+      'I am also an AWS Certified Developer and Solutions Architect with experience designing, developing and deploying AI and Big Data ' +
+      'solutions in a high paced agile environment. I am an avid clean coder who is adept at rapid prototyping for fast turnaround times in POCs.\n' +
       'I have rich experience with all stages of the software development life cycle, ' +
-      'more than 3 years of progressive experience in Java, Apache Hadoop, Big Data, HDFS, HBase, NiFi, Angular, HTML, CSS ' +
+      'around 4 years of progressive experience in Java, C#, Apache Hadoop, Big Data, HDFS, HBase, NiFi, Angular, HTML, CSS ' +
       'and Typescript development. \n' +
       'I have been working in an Agile environment and possess creative design thinking skills and dedicated to work effectively in ' +
       'dynamic environments. \n' +
