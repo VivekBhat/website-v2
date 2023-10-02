66f2c18361a74da848be9ae97e20fee7e6cb52b6
Vivek Bhat <bhatvivek93@gmail.com>
2023-10-02 11:08:09 -0700

    Adding SSD team

--- src/app/services/about/mock-about.service.ts
+++ src/app/services/about/mock-about.service.ts
@@ -6,12 +6,13 @@ import {Injectable} from '@angular/core';
 export class MockAboutService implements AboutService {
 
   getAbout(): Observable<string[]> {
+
     const about =
-      'I am a software engineer working for Amazon and write clean, scalable and tolerant microservice for the Wide Area Networking team of Azure. '+
+      'I am a software engineer working for Amazon and write clean, scalable and tolerant microservice for the Amazon Same Day Delivery team. '+
       'I am also an AWS Certified Developer and Solutions Architect with experience designing, developing and deploying AI and Big Data ' +
       'solutions in a high paced agile environment. I am an avid clean coder who is adept at rapid prototyping for fast turnaround times in POCs.\n' +
-      'I have rich experience with all stages of the software development life cycle, ' +
-      'around 4 years of progressive experience in Java, C#, Apache Hadoop, Big Data, HDFS, HBase, NiFi, Angular, HTML, CSS ' +
+      'I have extensive experience with all stages of the software development life cycle, ' +
+      '6+ years of progressive experience in Java, AWS, Apache Hadoop, Big Data, HDFS, HBase, NiFi, Angular, HTML, CSS ' +
       'and Typescript development. \n' +
       'I have been working in an Agile environment and possess creative design thinking skills and dedicated to work effectively in ' +
       'dynamic environments. \n' +
